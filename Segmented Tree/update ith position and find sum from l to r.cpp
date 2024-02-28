c1b90c66cb8c4c88e507c556e3536f42389a5de6
Md Ruhan Azad <ruhanazad007@gmail.com>
2024-02-28 15:02:44 +0600

    Update and rename update ith position and find sum from l to r to update ith position and find sum from l to r.cpp

--- Segmented Tree/update ith position and find sum from l to r.cpp
+++ Segmented Tree/update ith position and find sum from l to r.cpp
@@ -14,7 +14,7 @@ void init(int node, int lo, int hi){
     init(left,lo,mid);
     init(right,mid+1,hi);
 
-    tree[node]=tree[left]+tree[right];
+    tree[node]=tree[left]+tree[right]; //change this
     return;
 }
 
@@ -30,12 +30,12 @@ void update(int node, int lo, int hi, int pos, int newvalue){
     update(left,lo,mid,pos,newvalue);
     update(right,mid+1,hi,pos,newvalue);
 
-    tree[node]=tree[left]+tree[right];
+    tree[node]=tree[left]+tree[right]; // change this
     return;
 }
 
 int query(int node, int lo, int hi, int i, int j){
-    if(lo > j || hi < i) return 0;
+    if(lo > j || hi < i) return 0; // return appropriate value
     if(lo>=i && hi <= j) return tree[node];
 
     int left=node*2;
@@ -44,18 +44,18 @@ int query(int node, int lo, int hi, int i, int j){
     int res1=query(left,lo,mid,i,j);
     int res2=query(right,mid+1,hi,i,j);
 
-    return res1+res2;
+    return res1+res2; // change this
 }
 
 int main(){
     int n; cin>>n;
     for(int i=1; i<=n; i++) cin>>arr[i];
-    init(1,1,n);
+    init(1,1,n); // building the tree
 
     int pos,val; cin>>pos>>val;
-    update(1,1,n,pos,val);
+    update(1,1,n,pos,val); // update a single index
     int l,r; cin>>l>>r;
-    cout<<"Sum from "<<l<<" to "<<r<<" is: "<<query(1,1,n,l,r)<<endl;
+    cout<<"Sum from "<<l<<" to "<<r<<" is: "<<query(1,1,n,l,r)<<endl; // print the sum of a given range
 
 
     return 0;

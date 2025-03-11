5d16b373284e096f2e5f4e63f6b02f86ac035ca1
Md Ruhan Azad <ruhanazad007@gmail.com>
2025-03-11 13:25:12 +0600

    Update DSU.cpp

--- Data Structure/DSU.cpp
+++ Data Structure/DSU.cpp
@@ -14,3 +14,41 @@ void union_sets(int a, int b) {
     if (a != b)
         parent[b] = a;
 }
+
+
+/*In this implementation on union set the second tree always attached to the first one. Sometimes it can lead to trees containing chains of length of O(n). 
+With optimization we can choose which tree will get attached. We can use size of tree or depth of tree*/ 
+
+// By size of the tree
+void make_set(int v) {
+    parent[v] = v;
+    size[v] = 1;
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b)
+        if(size[a] < size[b]) swap(a, b);
+        parent[b] = a;
+        size[a] += size[b];
+}
+
+
+// By depth of the tree
+void make_set(int v) {
+    parent[v] = v;
+    rank[v] = 0;
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (rank[a] < rank[b])
+            swap(a, b);
+        parent[b] = a;
+        if (rank[a] == rank[b])
+            rank[a]++;
+    }
+}

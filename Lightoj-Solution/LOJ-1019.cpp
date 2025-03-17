5dcc7e59ffb41b5631d895195be3460b89fbc615
Md Ruhan Azad <ruhanazad007@gmail.com>
2025-03-17 19:04:10 +0600

    Create LOJ-1019.cpp

---
+++ Lightoj-Solution/LOJ-1019.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fastio std::ios_base::sync_with_stdio (false); cin.tie(0); cout.tie(0);
+
+const int N = 1e5 + 5;
+vector<pair<int, int>>adj[N];
+int n, e, dis[N];
+int INF = 1e9;
+
+void clr(){
+	for(int i = 0; i <= n; i++){
+		adj[i].clear();
+		dis[i] = INF;
+	}
+}
+
+void dijkstra(int s){
+	dis[s] = 0;
+	priority_queue<pair<int, int>>pq;
+	pq.push({s, 0});
+	
+	while(!pq.empty()){
+		auto [u, d] = pq.top(); pq.pop();
+		d = -d;
+		for(auto [v, w] : adj[u]){
+			if(dis[u] + w < dis[v]){ //dis[u] == d
+				dis[v] = dis[u] + w;
+				pq.push({v, -dis[v]});
+			}
+		}
+	}
+}
+
+void solve(){
+   cin >> n >> e;
+   for(int i = 0; i < e; i++){
+   	int u, v, w;
+   	cin >> u >> v >> w;
+   	adj[u].push_back({v, w});
+   	adj[v].push_back({u, w});
+   }
+   
+   for(int i = 0; i <= n; i++) dis[i] = INF;
+   dijkstra(1);
+   
+   if(dis[n] == INF) cout << "Impossible"<<endl;
+   else cout << dis[n] << endl;
+   
+   clr();
+}
+
+int main(){
+    int t=1, tc=1;
+    cin>>t;
+    while(t--){
+        cout << "Case " << tc++ << ": ";
+        solve();
+    }
+
+    return 0;
+}

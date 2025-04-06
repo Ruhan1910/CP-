ead0da31833945f8b56c3249950ccac39e4a4013
Md Ruhan Azad <ruhanazad007@gmail.com>
2025-04-06 20:58:18 +0600

    Create Knapsack_1_iterative(nw).cpp

---
+++ DP/Knapsack_1_iterative(nw).cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define set(n, k) (n | (1LL << k))
+#define unset(n, k) (n & ~(1LL << k))
+#define check(n , k) (n & (1LL << k))
+#define fastio std::ios_base::sync_with_stdio (false); cin.tie(0); cout.tie(0);
+
+ll n, w, res, inf = 1e18;
+vector<ll>weight(101), value(101);
+ll dp[101][100001];
+
+void solve(){
+    cin >> n >> w;
+    for(int i = 1; i <= n; i++){
+        cin >> weight[i] >> value[i];
+    }
+
+    for(int i = 0; i <= w; i++){
+        dp[0][i] = 0;
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = 0; j <= w; j++){
+            dp[i][j] = dp[i - 1][j];
+            if(j >= weight[i]){
+                dp[i][j] = max(dp[i][j], value[i] + dp[i - 1][j - weight[i]]);
+            }
+        }
+    }
+
+    cout << dp[n][w] << endl;
+}
+
+int main(){
+    fastio;
+    int t=1, tc=1;
+    //cin>>t;
+    while(t--){
+        //cout << "Case " << tc++ << ": ";
+        solve();
+    }
+
+    return 0;
+}

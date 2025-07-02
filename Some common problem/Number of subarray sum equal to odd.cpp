97fdabdee8b38e5ceb83a9c5e07c7d4e29c408d9
Md Ruhan Azad <ruhanazad007@gmail.com>
2025-07-02 14:43:53 +0600

    Update Number of subarray sum equal to odd.cpp

--- Some common problem/Number of subarray sum equal to odd.cpp
+++ Some common problem/Number of subarray sum equal to odd.cpp
@@ -1,6 +1,7 @@
 int numOfSubarrays(vector<int>& arr){
     int mod = 1e9 + 7;
-    map<int, int>mp;
+    map<int, int>mp; 
+    mp[0] = 1;
     int sum = 0, res = 0;
     for(int i = 0; i < arr.size(); i++){
         sum += arr[i];

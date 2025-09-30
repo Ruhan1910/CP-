5f2d752c0b86ab5cd42f6eb44c355d38b38f6357
Md Ruhan Azad <ruhanazad007@gmail.com>
2025-09-30 13:47:58 +0600

    Update Two line intersect.cpp

--- Geometry/Two line intersect.cpp
+++ Geometry/Two line intersect.cpp
@@ -4,7 +4,7 @@ struct Point{
 };
 
 int orientation(Point a, Point b, Point c){ //pii means pair<int, int>
-    ll val = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x); // cross product of ab and bc vector
+    ll val = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x); // cross product of ab and ac vector
 
     if(val == 0) return 0; //collinear
     if(val < 0) return 1; //clockwise
